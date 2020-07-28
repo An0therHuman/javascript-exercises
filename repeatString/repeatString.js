a9591ac4a9f024c1a6e25c8042fe71102aca110f
C0ldHands <leeyuenhigh@gmail.com>
2020-07-28 17:07:19 -0400

    Successfully completed all test

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(aString, numTimes) {
+    let repeatedString = '';
+    if (numTimes < 0){
+        repeatedString = 'ERROR';
+    }else{
+        for(i = 0; i < numTimes; i++){
+            repeatedString += aString;
+        }
+    }
+    return repeatedString;
 }
 
 module.exports = repeatString
