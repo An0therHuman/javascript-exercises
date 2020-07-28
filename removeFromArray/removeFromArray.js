09934795101b1504d4519914297d32a6f198a122
C0ldHands <leeyuenhigh@gmail.com>
2020-07-28 18:46:39 -0400

    Successfully completed all test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(aArray, ...valueToRemove) {
+    for (i = 0; i < aArray.length; i++){
+        for (k = 0; k < valueToRemove.length; k++){
+            if (aArray[i] === valueToRemove[k]){
+                // console.log(aArray[i], valueToRemove[k]);
+                aArray.splice(i,1);
+                i = 0;
+            }else{
+                // console.log('Else statement', aArray[i], valueToRemove[k]);
+                continue;
+            }
+        }
+    }
+    return aArray;
 }
 
 module.exports = removeFromArray
