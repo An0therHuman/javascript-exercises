d38393cfa39edf7c3b23efca0e89cb667dbc28a8
C0ldHands <leeyuenhigh@gmail.com>
2020-07-28 17:29:33 -0400

    Successfully completed all tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(aString) {
+    let reversedString = '';
+    let modifiedString = aString;
+    for (i = 0; i < aString.length; i++){
+        reversedString += modifiedString.slice(modifiedString.length-1);
+        modifiedString = modifiedString.slice(0,modifiedString.length-1);
+    }
+    return reversedString;
 }
 
 module.exports = reverseString
