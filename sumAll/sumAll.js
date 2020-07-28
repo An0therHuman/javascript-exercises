99b4c0172e69d79e781aca5f2a42d16e549cb9c3
C0ldHands <leeyuenhigh@gmail.com>
2020-07-28 19:27:29 -0400

    Successfully completed all test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startValue, endValue) {
+    let finalSum = 0;
+    if (startValue < 0 || endValue <0){
+        finalSum = 'ERROR';
+    }else if ((typeof startValue != "number")|| (typeof endValue != "number")){
+        finalSum = 'ERROR';
+    }else if (startValue > endValue){
+        let newStartValue = endValue;
+        let newEndValue = startValue;
+        for (i = newStartValue; i <= newEndValue; i++){
+            finalSum += i;
+        }
+    }else{
+        for (i = startValue; i <= endValue; i++){
+            finalSum += i;
+        }
+    }
+    return finalSum;
 }
 
 module.exports = sumAll
