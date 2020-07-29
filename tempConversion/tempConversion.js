8f755d3d6dc6dd511e00ea914626778b0a57cd15
C0ldHands <leeyuenhigh@gmail.com>
2020-07-28 20:06:14 -0400

    Completed all test

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  let tempF = (tempFahrenheit - 32) * (5/9);
+  return Math.round(tempF * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  let tempC = (tempCelsius * (9/5)) + 32;
+  return Math.round(tempC * 10) / 10;
 }
 
 module.exports = {
