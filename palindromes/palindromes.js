aecc9f040dbc74ed952da5fd7d6120b65b896d16
C0ldHands <leeyuenhigh@gmail.com>
2020-08-01 23:10:58 -0400

    Successfully completed all test

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
+const palindromes = function(aString) {
+    aString = aString.toLowerCase();
+    let text = aString.split("");
+    const punctuation = ['.',',','!',' '];
+    let initalString = ''
+    for (i = 0; i < text.length; i++){
+        if (!punctuation.includes(text[i])){
+            initalString += text[i];
+        };
+    }
 
+    let finalString = "";
+    for (i = initalString.length-1; i >= 0; i--){
+        finalString += initalString[i];
+    }
+    return initalString == finalString;
 }
 
 module.exports = palindromes
