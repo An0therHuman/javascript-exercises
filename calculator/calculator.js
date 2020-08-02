8fc9fb26fb99f2091d86ad485fc16ed1ccd3ea63
C0ldHands <leeyuenhigh@gmail.com>
2020-08-01 22:08:46 -0400

    Successfully completed all test

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,42 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2
 }
 
-function sum () {
-	
+function sum (num) {
+	let total = num.reduce((total, num) => {
+		return total + num;
+	},0);
+	// let total = 0;
+	// for (i = 0; i < num.length; i++){
+	// 	total += num[i];
+	// }
+	return total;
 }
 
-function multiply () {
-	
+function multiply (num) {
+	if (num.length != 0){
+		let product = num.reduce((product, num) => {
+			return product * num;
+		});
+		return product;
+	}
 }
 
-function power() {
-	
+function power(num, power) {
+	return num ** power;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let product = 1;
+	while (num > 0){
+		product *= num;
+		num--
+	}
+	return product;	
 }
 
 module.exports = {
