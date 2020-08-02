7c63be0cc8d24f65587f32d544ff3467aa7b71f4
C0ldHands <leeyuenhigh@gmail.com>
2020-08-02 18:13:41 -0400

    Used one built in js method

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,9 +1,12 @@
 const getTheTitles = function(arrayBooks) {
-    let books = [];
-    for (i = 0; i < arrayBooks.length; i++){
-        books.push(arrayBooks[i].title)
-    }
-    return books;
+    // let books = [];
+    // for (i = 0; i < arrayBooks.length; i++){
+    //     books.push(arrayBooks[i].title)
+    // }
+    // return books;
+
+    //Does everything above, but looks nicer
+    return arrayBooks.map(book => book.title)
 }
 
 module.exports = getTheTitles;
