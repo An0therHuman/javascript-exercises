658f04d44f62d19a543beadc5e289a6a5cba2a1f
C0ldHands <leeyuenhigh@gmail.com>
2020-08-02 18:10:13 -0400

    Successfully completed the test

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayBooks) {
+    let books = [];
+    for (i = 0; i < arrayBooks.length; i++){
+        books.push(arrayBooks[i].title)
+    }
+    return books;
 }
 
 module.exports = getTheTitles;
