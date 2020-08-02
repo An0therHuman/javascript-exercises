103d71e72e9bcbb73cef5a9a9379ff1795afb62c
C0ldHands <leeyuenhigh@gmail.com>
2020-08-02 18:01:25 -0400

    Successfully completed all test

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let previousValue = 1;
+    let previousValue2 = 1;
+    let currentValue;
+    if (number > 2){
+        for(i =2; i < number; i ++){
+            currentValue = previousValue + previousValue2;
+            previousValue2 = previousValue;
+            previousValue = currentValue;
+            
+        }
+    }else if (number > 0){
+        currentValue = 1;
+    }else{
+        currentValue = 'OOPS'
+    }
+    return currentValue;
 }
+
+console.log(fibonacci(6))
 
 module.exports = fibonacci
