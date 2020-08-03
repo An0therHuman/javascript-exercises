45b340e358d5b53380f8429535d5745c4ffa413a
C0ldHands <leeyuenhigh@gmail.com>
2020-08-03 00:04:28 -0400

    Replaced if/else with a ternary operator

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -2,17 +2,9 @@ let findTheOldest = function(arrayPeople) {
     const currentYear = 2020;
     let sortedPeople = arrayPeople.sort((person1, person2) => {
         if (person1.hasOwnProperty('yearOfDeath') && person2.hasOwnProperty('yearOfDeath')){
-            if ((person1.yearOfDeath - person1.yearOfBirth) > (person2.yearOfDeath - person2.yearOfBirth)){
-                return -1;
-            }else{
-                return 1;
-            }
+            return (person1.yearOfDeath - person1.yearOfBirth) > (person2.yearOfDeath - person2.yearOfBirth) ? -1 : 1;
         }else{
-            if ((currentYear - person1.yearOfBirth) > (currentYear - person2.yearOfBirth)){
-                return -1;
-            }else{
-                return 1;
-            }
+            return (currentYear - person1.yearOfBirth) > (currentYear - person2.yearOfBirth) ? -1 : 1;
         }
     })
     let oldestPerson = sortedPeople[0];
